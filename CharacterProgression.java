d0d798fccfb3ac3a0d36229008c0503cd2bcd0b4
Davide Tansini <davide.tansini87@gmail.com>
2024-02-17 18:45:56 +0100

    si comincia a fare il refactoring del codice

---
+++ CharacterProgression.java
@@ -0,0 +1,8 @@
+public interface CharacterProgression {
+
+    // qui copio i medoti di CharacterProgressionClass e cerco poi di rewoekare il tutto in ottica scalabile
+    // così diventa la base per la progressione di qualsiasi classe
+
+
+
+}
